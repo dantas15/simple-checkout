0cd7faae36c60207bb0f51f00361c062dc02e548
Gustavo Dantas <gusdantas15@gmail.com>
2024-07-12 00:14:46 -0300

    chore(web): add select-amount #2

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,48 @@
+import { Send } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  FormControl,
+  InputAdornment,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { MainWrapper } from '@simple-checkout/ui/components';
 
 export default function Home() {
+  const error = false;
+  const helperText = error ? 'error!' : '';
+
   return (
     <MainWrapper alignCenter>
-      <h1>Hello World!</h1>
+      <form>
+        <Typography variant="h4" gutterBottom>
+          Quanto você vai pagar?
+        </Typography>
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <FormControl>
+            <TextField
+              id="amount"
+              error={error}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              helperText={helperText}
+              placeholder="0.0"
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            disabled={true}
+            variant="contained"
+            startIcon={<Send />}
+          >
+            Enviar
+          </Button>
+        </Box>
+      </form>
     </MainWrapper>
   );
 }
