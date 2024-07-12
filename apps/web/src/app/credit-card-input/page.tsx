db835f0da78216c9f0a772c24c936a2a27a876ce
Gustavo Dantas <gusdantas15@gmail.com>
2024-07-12 01:34:58 -0300

    chore(web): add form validation

---
+++ apps/web/src/app/credit-card-input/page.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+export default function CreditCardInput() {
+  return <Typography>oiii</Typography>;
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,45 +1,86 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Send } from '@mui/icons-material';
 import {
   Box,
   Button,
+  CircularProgress,
   FormControl,
   InputAdornment,
   TextField,
   Typography,
 } from '@mui/material';
 import { MainWrapper } from '@simple-checkout/ui/components';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const amountSchema = z.object({
+  amount: z.coerce
+    .number({ message: 'A quantidade deve ser um número' })
+    .int({ message: 'A quantidade deve ser um número inteiro' })
+    .min(1, { message: 'A quantidade deve ser maior ou igual a 1' }),
+});
 
 export default function Home() {
-  const error = false;
-  const helperText = error ? 'error!' : '';
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof amountSchema>>({
+    resolver: zodResolver(amountSchema),
+  });
+
+  const handleOnSubmit = async (data: z.infer<typeof amountSchema>) => {
+    setIsLoading(true);
+    setTimeout(() => {
+      alert(data.amount);
+      router.push('/credit-card-input');
+      setIsLoading(false);
+    }, 1000);
+  };
 
   return (
     <MainWrapper alignCenter>
-      <form>
+      <form onSubmit={form.handleSubmit(handleOnSubmit)}>
         <Typography variant="h4" gutterBottom>
           Quanto você vai pagar?
         </Typography>
         <Box sx={{ display: 'flex', gap: 2 }}>
           <FormControl>
             <TextField
-              id="amount"
-              error={error}
+              {...form.register('amount')}
+              error={!!form.formState.errors.amount}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">$</InputAdornment>
                 ),
               }}
-              helperText={helperText}
+              helperText={form.formState.errors.amount?.message}
               placeholder="0.0"
             />
           </FormControl>
           <Button
             type="submit"
-            disabled={true}
+            disabled={isLoading}
             variant="contained"
             startIcon={<Send />}
           >
             Enviar
+            {isLoading && (
+              <CircularProgress
+                size={24}
+                sx={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  marginTop: '-12px',
+                  marginLeft: '-12px',
+                }}
+              />
+            )}
           </Button>
         </Box>
       </form>
