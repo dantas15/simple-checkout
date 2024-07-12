8443b9c4dc89d0d34efee8b03136bfae1f867495
Gustavo Dantas <gusdantas15@gmail.com>
2024-07-12 08:00:06 -0300

    chore: update dayjs locale

--- apps/web/src/app/client-providers.tsx
+++ apps/web/src/app/client-providers.tsx
@@ -3,13 +3,14 @@
 import { ReactNode } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/pt-br';
 
 type Props = {
   children: ReactNode;
 };
 export function ClientProviders({ children }: Props) {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
       {children}
     </LocalizationProvider>
   );
