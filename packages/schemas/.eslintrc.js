e0bb9c9e12385d8ca9207a26c390f43d30c3c421
Gustavo Dantas <gusdantas15@gmail.com>
2024-07-16 16:01:32 -0300

    feat(schemas): create empty schemas package

---
+++ packages/schemas/.eslintrc.js
@@ -0,0 +1,6 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ['@simple-checkout/eslint-config/library.js'],
+  parser: '@typescript-eslint/parser',
+};
