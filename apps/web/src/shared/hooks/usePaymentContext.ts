f30224d2bc19fcf64573848cbcebaa8a4950b38c
Gustavo Dantas <gusdantas15@gmail.com>
2024-07-16 07:57:49 -0300

    chore(payment-context): add isBrowser on usePaymentContext to prevent compilation errors from Next

--- apps/web/src/shared/hooks/usePaymentContext.ts
+++ apps/web/src/shared/hooks/usePaymentContext.ts
@@ -3,8 +3,27 @@ import {
   PaymentContext,
   PaymentContextType,
 } from '../contexts/payment-context';
+import { isBrowser } from '../../utils/isBrowser';
+
+const paymentContextNullValues: PaymentContextType = {
+  paymentStatus: 'pending',
+  user: null,
+  updateUser: async () => {},
+  amount: null,
+  updateAmount: async () => {},
+  creditCard: null,
+  updateCreditCard: async () => {},
+  pixPreference: null,
+  updatePixPreferences: async () => {},
+  updatePixPayment: async () => {},
+  clearData: () => {},
+  isPaymentLoading: true,
+};
 
 export function usePaymentContext(): PaymentContextType {
+  if (!isBrowser()) {
+    return paymentContextNullValues;
+  }
   const context = useContext(PaymentContext);
   if (context === undefined) {
     throw new Error('usePayment must be used within a PaymentProvider');
